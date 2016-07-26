8bd86eb9f22cdc5ce072201bd444edf8c5c39ff1
Peter <ptc62@msn.com>
2016-07-25 20:24:19 -0400

    still working

--- taxes.py
+++ taxes.py
@@ -6,10 +6,6 @@
 import numpy as np
 import matplotlib.pyplot as plot
 
-"""
-Find annual population growth
-"""
-
 
 # Function that will plot graphs when provided a x and y vector
 def drawPlot(x, y, ylimit=[0, 10000000]):
@@ -23,6 +19,18 @@ def drawPlot(x, y, ylimit=[0, 10000000]):
     plot.grid()
     plot.show()
 
+
+# Function to calculate the taxes taken by the three major State revenue streams
+def calcrevenue(totalSpent, percentSalesContribution, percentIncomeContribution):
+    amatrix = np.array([[1, 1, 1], [0, 1, 0], [1, 0, 0]])
+    bmatrix = np.array([totalSpent, (totalSpent * percentIncomeContribution), (totalSpent * percentSalesContribution)])
+    xmatrix = np.linalg.solve(amatrix, bmatrix)
+    return xmatrix
+
+"""
+Find annual population growth
+"""
+
 # Historical Georgia Population
 ga_census = [[1790, 1800, 1810, 1820, 1830, 1840, 1850, 1860, 1870, 1880, 1890, 1900,
               1910, 1920, 1930, 1940, 1950, 1960, 1970, 1980, 1990, 2000, 2010],
@@ -31,7 +39,7 @@ ga_census = [[1790, 1800, 1810, 1820, 1830, 1840, 1850, 1860, 1870, 1880, 1890, 
               3943116, 4589575, 5463105, 6478216, 8186453, 9687653]]
 
 # Show Current Population Growth Graph
-# drawPlot(ga_census[0], ga_census[1])
+drawPlot(ga_census[0], ga_census[1])
 
 # Population numpy array
 population = np.array(ga_census[1])
@@ -56,7 +64,7 @@ print(poly)
 yc = np.polyval(poly, x)
 
 # Plot our curve fitted polynomial
-# drawPlot(ga_census[0], yc)
+drawPlot(ga_census[0], yc)
 
 new_xf = len(population) + 1  # Plus one to predict population up to the year 2020
 num_x = 220  # Number of years between 2010 and 1790
@@ -67,7 +75,7 @@ new_x = np.linspace(xi, new_xf, num_x)
 predict_pop = np.polyval(poly, new_x)
 
 # Plot predicted annual population values to 2020
-# drawPlot(new_x, predict_pop, ylimit=[0,14000000])
+drawPlot(new_x, predict_pop, ylimit=[0, 14000000])
 
 
 """
@@ -83,7 +91,7 @@ ga_unemployment_rate = np.array([4.717, 4.625, 6.475, 10.042, 10.392, 10.058, 9.
                                  7.017, 5.825])
 
 # Plot historical data
-# drawPlot(ga_labor_force[0], ga_labor_force[1], ylimit=[4000000, 5500000])
+drawPlot(ga_labor_force[0], ga_labor_force[1], ylimit=[4000000, 5500000])
 
 # Attempt to find a polynomial through curvefitting
 labor_x = np.linspace(xi, len(ga_labor_force[1]), len(ga_labor_force[1]))
@@ -91,19 +99,31 @@ poly = np.polyfit(labor_x, ga_labor_force[1], 3)
 labor_y = np.polyval(poly, labor_x)
 
 # Plot the curve fitted polynomial to see how close to historical data it is
-# drawPlot(labor_x, labor_y, ylimit=[4000000, 5500000])
+drawPlot(labor_x, labor_y, ylimit=[4000000, 5500000])
 
 # Plot additinal data point to forecast labor force
 labor_x_predict = np.linspace(xi, 15, 15)
 predict_labor_y = np.polyval(poly, labor_x_predict)
-# drawPlot(labor_x_predict, predict_labor_y)
+drawPlot(labor_x_predict, predict_labor_y)
+
+"""
+Total Spending by Georgia
+"""
+
+# Georgia Spending from 2010 - 2016, in Billions (State & Local Governments)
+ga_spending = [78.2, 78.5, 78.4, 78.8, 78.4, 78.2]
 
 
 
 
-# TESTING
-amatrix = np.array([[1, 1, 1], [9, 4, 0]])
-bmatrix = np.array([78.2, 62.56])
-xmatrix = np.linalg.solve(amatrix, bmatrix)
 
-print(xmatrix)
+"""
+Tax breakdown and predictions
+"""
+
+# Sales tax from
+Sales_Tax
+
+year2016 = calcrevenue(79, .29, .51)
+
+print(year2016)
